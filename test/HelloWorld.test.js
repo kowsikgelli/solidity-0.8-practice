154f508225f43a5173f52e304fff3a3546e0ae72
kowsikgelli <jaikrishnagelli@gmail.com>
2022-03-30 14:56:29 +0530

    hello world with test

---
+++ test/HelloWorld.test.js
@@ -0,0 +1,9 @@
+const HelloWorld = artifacts.require("HelloWorld");
+
+contract('HelloWorld',()=>{
+    it('message value should be Hello World',async ()=>{
+        const instance = await HelloWorld.new();
+        const message = await instance.message();
+        assert(message == "Hello World");
+    })
+})
